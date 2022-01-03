b5b9409625063a1882afc976130c1a8f0a1c1939
Tasha Syarifah <tashadifa@gmail.com>
2022-01-03 13:28:21 +0700

    Update index and App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import Navbar from "./components/Navbar"
+import Card from "./components/Card";
+
+function App() {
+    return (
+        <div className="App">
+            <Navbar />
+            <Card />
+        </div>
+    );
+}
+
+export default App;
