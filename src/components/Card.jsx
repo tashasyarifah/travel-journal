54264601abb59606fd82cd5d4ab40433924f8c99
Tasha Syarifah <tashadifa@gmail.com>
2022-01-06 15:31:15 +0700

    Update styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,14 +6,15 @@ export default function Card(props) {
             <img src={`../images/${props.image}`} alt="The destination" className={styles.img}/>
             <div className={styles.info}>
                 <div className={styles.locations}>
-                    <img src="./images/Location.svg" alt="" />
-                    <span>{props.title}</span>
-                    <span>{props.googleMapsUrl}</span>
+                    <img src="./images/Location.svg" alt="" className={styles.icon}/>
+                    <span className={styles.country}>{props.location}</span>
+                    <a href="/">View in Google Maps</a>
                 </div>
                 <h2>{props.title}</h2>
-                <p>{props.startDate} - {props.endDate}</p>
-                <p>{props.description}</p>
+                <p className={styles.date}>{props.startDate} - {props.endDate}</p>
+                <p className={styles.description}>{props.description}</p>
             </div>
+            <hr />
             
         </div>
     )
