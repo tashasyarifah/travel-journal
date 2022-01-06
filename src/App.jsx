029d7038d40cdd1707b4059e4449c047d499f912
Tasha Syarifah <tashadifa@gmail.com>
2022-01-06 15:29:07 +0700

    Solve the issue and remove the console.log

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import Navbar from "./components/Navbar";
 import Card from "./components/Card";
 import Data from "./Data";
 import "./App.css";
-console.log(Data);
+
 
 export default function App() {
   const cards = Data.map((item) => {
     return <Card key={item.id} {...item} />;
   });
   return (
-    <div>
-      <Navbar />
-      {cards}
+    <div className="App">
+        <Navbar />
+        {cards}
     </div>
   );
 }
