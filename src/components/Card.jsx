e3d15d208c23f78c8a742146a31bb14c438598ae
Tasha Syarifah <tashadifa@gmail.com>
2022-01-07 20:35:53 +0700

    Create a new section to add hr tag

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,20 +2,23 @@ import styles from "./Card.module.css"
 
 export default function Card(props) {
     return (
-        <div className={styles.card}>
-            <img src={`../images/${props.image}`} alt="The destination" className={styles.img}/>
-            <section className={styles.info}>
-                <section className={styles.locations}>
-                    <img src="./images/Location.svg" alt={props.altImage} className={styles.icon}/>
-                    <span className={styles.country}>{props.location}</span>
-                    <a target="_blank" rel="noopener noreferrer" href={props.googleMapsUrl} className={styles.url}>View in Google Maps</a>
+        <section>
+            <div className={styles.card}>
+                <img src={`../images/${props.image}`} alt="" className={styles.img}/>
+                <section className={styles.info}>
+                    <section className={styles.locations}>
+                        <img src="./images/Location.svg" alt={props.altImage} className={styles.icon}/>
+                        <span className={styles.country}>{props.location}</span>
+                        <a target="_blank" rel="noopener noreferrer" href={props.googleMapsUrl} className={styles.url}>View in Google Maps</a>
+                    </section>
+                    <h2>{props.title}</h2>
+                    <p className={styles.date}>{props.startDate} - {props.endDate}</p>
+                    <p className={styles.description}>{props.description}</p>
                 </section>
-                <h2>{props.title}</h2>
-                <p className={styles.date}>{props.startDate} - {props.endDate}</p>
-                <p className={styles.description}>{props.description}</p>
-            </section>
+            </div>
             <hr />
-            
-        </div>
+        </section>
+        
+        
     )
 }
