06d2457710b94603461a6ed0ac4ec57605c9f5db
Tasha Syarifah <tashadifa@gmail.com>
2022-01-08 17:54:09 +0700

    Remove className=attribution and its style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css"
 export default function Footer() {
     return (
         <section className={styles.footer}>
-            <p className={styles.attribution}>Made by Tasha for <a target="_blank" rel="noopener noreferrer" href="https://scrimba.com/learn/learnreact" className={styles.link}> Scrimba Solo Project </a>
+            <p>Made by Tasha for <a target="_blank" rel="noopener noreferrer" href="https://scrimba.com/learn/learnreact" className={styles.link}> Scrimba Solo Project </a>
             </p>
         </section>
     )
